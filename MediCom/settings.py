5e31bca431d72c59c426c08b301ed625e691ce7f
huynguyen-py <iamhuynguyen1002@gmail.com>
2020-09-14 13:39:50 +0700

    Our first commit

--- MediCom/settings.py
+++ MediCom/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_URL = [
     os.path.join(BASE_DIR, 'static'),
 ]
